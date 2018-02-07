024e55c1382f1104a512a06c0cb2d1ea60d1816a
Andrei Frolov <frolov@sfu.ca>
2018-02-07 15:48:25 -0800

    Pendulum view added to the main storyboard.

--- Double Pendulum/Double Pendulum/PendulumView.swift
+++ Double Pendulum/Double Pendulum/PendulumView.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class PendulumView: UIView {
 
-    /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        StyleKit.drawDoublePendulum(frame: rect, phi: 45, psi: -30)
     }
-    */
 
 }
