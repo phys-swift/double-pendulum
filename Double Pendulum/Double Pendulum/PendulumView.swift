b2046ba79af3e12fae406c4ef661f2797f5903c7
Andrei Frolov <frolov@sfu.ca>
2018-02-07 15:17:07 -0800

    PendulumView draws double pendulum.

---
+++ Double Pendulum/Double Pendulum/PendulumView.swift
@@ -0,0 +1,21 @@
+//
+//  PendulumView.swift
+//  Double Pendulum
+//
+//  Created by Andrei Frolov on 2018-02-07.
+//  Copyright © 2018 SFU Physics Department. All rights reserved.
+//
+
+import UIKit
+
+class PendulumView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
