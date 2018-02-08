ab77c11d81343ca0a490c16e0ce73cd6430be92d
Andrei Frolov <frolov@sfu.ca>
2018-02-08 13:05:43 -0800

    Animated pendulum view with updates driven by display refresh link.

--- Double Pendulum/Double Pendulum/PendulumView.swift
+++ Double Pendulum/Double Pendulum/PendulumView.swift
@@ -12,10 +12,29 @@ class PendulumView: UIView {
     @objc dynamic var phi: Float = 0.0 { didSet { if phi != oldValue { setNeedsDisplay() } } }
     @objc dynamic var psi: Float = 0.0 { didSet { if psi != oldValue { setNeedsDisplay() } } }
     
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    // link to display refresh rate timer
+    var link: CADisplayLink? = nil { didSet {
+        guard link != oldValue else { return }
+        if let link = oldValue { link.invalidate() }
+        if let link = link { link.add(to: .current, forMode: .defaultRunLoopMode) }
+    } }
+    
+    // clean up
+    deinit { link = nil }
+    
+    // runtime initialization
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let link = CADisplayLink(target: self, selector: #selector(step))
+        link.preferredFramesPerSecond = 30; self.link = link
+    }
+    
+    // draw double pendulum
     override func draw(_ rect: CGRect) {
         StyleKit.drawDoublePendulum(frame: rect, phi: CGFloat(phi), psi: CGFloat(psi))
     }
-
+    
+    // pendulum state update
+    @objc func step() { phi += 0.5; psi += 1.3 }
 }
