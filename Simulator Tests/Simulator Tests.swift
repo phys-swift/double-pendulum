70d160475de732e3d74f33bf2bc995a0c0276f9e
Andrei Frolov <frolov@sfu.ca>
2018-02-11 03:01:20 -0800

    Simulator test target created; GL8 integrator tested.

---
+++ Simulator Tests/Simulator Tests.swift
@@ -0,0 +1,55 @@
+//
+//  Simulator Tests.swift
+//  Simulator Tests
+//
+//  Created by Andrei Frolov on 2018-02-11.
+//  Copyright © 2018 SFU Physics Department. All rights reserved.
+//
+
+import XCTest
+import simd
+
+class SimulatorTests: XCTestCase {
+    // setup and teardown
+    override func setUp() { super.setUp() }    
+    override func tearDown() { super.tearDown() }
+    
+    // test tolerance
+    let epsilon: Double = 1.0e-14
+    let delta: Double = 1.0e-2
+    
+    // test workload
+    #if DEBUG
+    let steps = 10000
+    #else
+    let steps = 1000000
+    #endif
+    
+    func testHarmonicOscillator() {
+        let dt: Double = 6.2831853071795864769252867665590057684/1024
+        var y = double4(1, 0, 0, 0)
+        
+        for _ in 0...10240 { y = gl8(state: y, step: dt) { y in double4(y[1], -y[0], 0, 0) } }
+        let q = y[0], p = y[1], dE = (p*p)/2.0 + (q*q)/2.0 - 0.5
+        
+        XCTAssertEqual(dE, 0.0, accuracy: epsilon)
+        XCTAssertEqual(p,  0.0, accuracy: delta)
+    }
+    
+    func testAnharmonicOscillator() {
+        let dt: Double = 7.416298709205487673735401388781040185/1024
+        var y = double4(1, 0, 0, 0)
+        
+        for _ in 0...10240 { y = gl8(state: y, step: dt) { y in double4(y[1], -y[0]*y[0]*y[0], 0, 0) } }
+        let q = y[0], p = y[1], dE = (p*p)/2.0 + (q*q*q*q)/4.0 - 0.25
+        
+        XCTAssertEqual(dE, 0.0, accuracy: epsilon)
+        XCTAssertEqual(p,  0.0, accuracy: delta)
+    }
+    
+    func testGL8Performance() {
+        let steps = self.steps, dt = 0.01; var y = double4(1, 0, 0, 0)
+        
+        self.measure { for _ in 0...steps { y = gl8(state: y, step: dt) { y in double4(y[1], -y[0]*y[0]*y[0], 0, 0) } } }
+    }
+}
