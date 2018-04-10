0249d1a15bd45720f7c3812a25ac8518b293522c
Andrei Frolov <frolov@sfu.ca>
2018-04-09 21:34:53 -0700

    First take at the attributed about blurb.

--- About.swift
+++ About.swift
@@ -8,17 +8,86 @@
 
 import UIKit
 
-let blurb: String = """
+// MARK: magic character representing attachment in attributed strings
+let attachment = Character(Unicode.Scalar(NSAttachmentCharacter)!)
 
-Double pendulum is a simple mechanical system displaying chaos.
+// MARK: plain text about blurb
+let blurb: String = """
+\(attachment)
+
+Double pendulum is a simple mechanical system displaying chaos. The motion of the pendulum arms can be complex and unpredictable, as even minute changes can lead to very different trajectories. This app accurately simulates physics of a frictionless double pendulum, so you can explore and enjoy complexity and beaty of mechanical chaos.
+
+Interacting with the pendulum:
+Short-tap with one finger to stop the pendulum, long-press to grab it by the end and drag around. Swipe within inner circle to give the first arm a kick, or within outer circle to give the second arm a kick in the direction of the swipe. Tap with two fingers to pause or resume the simulation. If motion of the pendulum gets too fast, emergency brakes will engage. You can control simulation using real gravity and your device orientation if you enable it in Settings.
+
+What exactly is being computed here?
+Equations of motion of a double pendulum are derived here. They are integrated numerically using eighth-order Gauss-Legendre method, which is highly accurate and preserves the symplectic form for Hamiltonian systems. Dragging the pendulum by the end actually solves equations of motion of a double pendulum with a spring attached between its end and your finger. Viscous friction used to damp the pendulum motion while dragging or braking is modelled by Rayleigh dissipation.
+
+Want to learn more?
+This app is brought to you by Cosmology Group at the Physics Department of the Simon Fraser University, where we teach classical and modern physics courses, as well as how to do computer simulations and write apps such as this one. Come to the Dark Side, we have cookies!
+
+Have Fun!
 """
 
+// MARK: attributed about blurb
 let about: NSAttributedString = {
+    // logo attachment
+    let logo = NSTextAttachment(); logo.image = UIImage(named: "SFU")
+    let w = UIScreen.main.fixedCoordinateSpace.bounds.width - 32, h = w/6.9
+    logo.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: w, height: h))
+    
+    // paragraph styles
+    let style = NSMutableParagraphStyle()
+    style.paragraphSpacing = 4; let normal = style.copy()
+    style.alignment = .center;  let center = style.copy()
+    
+    // common attributes
     let body: [NSAttributedStringKey: Any] = [
+        .paragraphStyle: normal,
         .font: UIFont.preferredFont(forTextStyle: .body)
     ]
     
+    let title: [NSAttributedStringKey: Any] = [
+        .paragraphStyle: center,
+        .font: UIFont.preferredFont(forTextStyle: .title1)
+    ]
+    
+    let heading: [NSAttributedStringKey: Any] = [
+        .font: UIFont.preferredFont(forTextStyle: .headline)
+    ]
+    
+    let gesture: [NSAttributedStringKey: Any] = [
+        .underlineStyle: NSUnderlineStyle.styleSingle.rawValue
+    ]
+    
+    // attributed about text
     let about = NSMutableAttributedString(string: blurb, attributes: body)
+    about.addAttribute(.attachment, value: logo, range: NSMakeRange(0,1))
+    
+    // dress up attributed text
+    for (tag,style) in [
+        "Interacting with the pendulum:": heading,
+        "What exactly is being computed here?": heading,
+        "Want to learn more?": heading,
+        "Have Fun!": title,
+        "short-tap": gesture,
+        "long-press": gesture,
+        "swipe": gesture,
+        "tap with two fingers": gesture,
+        "Settings": [NSAttributedStringKey.link: UIApplicationOpenSettingsURLString],
+        "derived here": [NSAttributedStringKey.link: "https://en.wikipedia.org/wiki/Double_pendulum"],
+        "Gauss-Legendre method": [NSAttributedStringKey.link: "https://doi.org/10.1090/S0025-5718-1964-0159424-9"],
+        "symplectic form": [NSAttributedStringKey.link: "https://en.wikipedia.org/wiki/Symplectic_integrator"],
+        "Hamiltonian systems": [NSAttributedStringKey.link: "https://en.wikipedia.org/wiki/Hamiltonian_mechanics"],
+        "Rayleigh dissipation": [NSAttributedStringKey.link: "https://en.wikipedia.org/wiki/Lagrangian_mechanics#Extensions_to_include_non-conservative_forces"],
+        "Cosmology Group": [NSAttributedStringKey.link: "http://www.sfu.ca/physics/cosmology/"],
+        "Physics Department": [NSAttributedStringKey.link: "https://www.sfu.ca/physics.html"],
+        "Simon Fraser University": [NSAttributedStringKey.link: "https://www.sfu.ca/"],
+        "physics courses": [NSAttributedStringKey.link: "https://www.sfu.ca/physics/courses/yearly-offerings.html"]
+    ] {
+        guard let range = blurb.range(of: tag, options: .caseInsensitive) else { continue }
+        about.addAttributes(style, range: NSMakeRange(range.lowerBound.encodedOffset, tag.count))
+    }
     
     return NSAttributedString(attributedString: about)
 }()
--- Double Pendulum/ViewController.swift
+++ Double Pendulum/ViewController.swift
@@ -17,11 +17,16 @@ class ViewController: UIViewController {
 // about page view controller
 class AboutController: UIViewController {
     @IBOutlet weak var about: UITextView!
+    static let blurb = Double_Pendulum.about
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        about.attributedText = Double_Pendulum.about
+        
+        // configure about view
+        about.attributedText = AboutController.blurb
         about.adjustsFontForContentSizeCategory = true
+        about.textContainerInset = UIEdgeInsets(top: 24, left: 8, bottom: 8, right: 8)
+        about.scrollRangeToVisible(NSMakeRange(0, 1))
     }
     
     override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
